68f45c606b48bea5dec615d34d21b8513b165474
Nigel Landwehr Johann <nigel_landwehr@hotmail.com>
2015-04-15 12:22:42 +0200

    Cleaned some code

--- SSLtool/app/src/main/java/com/example/securities/ssltool/MainActivity.java
+++ SSLtool/app/src/main/java/com/example/securities/ssltool/MainActivity.java
@@ -2,16 +2,11 @@ package com.example.securities.ssltool;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -49,6 +44,6 @@ public class MainActivity extends ActionBarActivity {
     public void goToHostname (View view){
         t = new TextView(this);
         t = (TextView)findViewById(R.id.textView2);
-        t.setText("String");
+        t.setText("Checking Hostname...");
     }
 }
