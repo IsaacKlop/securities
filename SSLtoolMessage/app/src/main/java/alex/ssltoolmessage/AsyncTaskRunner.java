3695a93b6fc5fe98e3ba7d9109cd9b70fcdebf71
Isaac Klop <Isaac.Klop@hva.nl>
2015-05-17 01:24:06 +0200

    App met login, inclusief SSL - werkt momenteel alleen lokaal bij mij

---
+++ SSLtoolMessage/app/src/main/java/alex/ssltoolmessage/AsyncTaskRunner.java
@@ -0,0 +1,34 @@
+package alex.ssltoolmessage;
+
+    import java.util.ArrayList;
+    import org.apache.http.NameValuePair;
+    import org.apache.http.message.BasicNameValuePair;
+    import android.os.AsyncTask;
+    import android.util.Log;
+
+
+public class AsyncTaskRunner extends  AsyncTask<String,String,String>{
+
+    private String response;
+    @Override
+    protected String doInBackground(String... params) {
+        int count = params.length;
+        if(count==2){
+            ArrayList<NameValuePair> postParameters = new ArrayList<>();
+            postParameters.add(new BasicNameValuePair("username",params[0]));
+            postParameters.add(new BasicNameValuePair("password",params[1]));
+            try {
+                response = SimpleHttpClient.executeHttpPost("https://192.168.43.1:8443/SSLTestServer/AppResponse", postParameters);
+                String res = response.toString();
+                Log.d("Response", res);
+                this.response = res.replaceAll("\\s+", "");
+            } catch (Exception e) {
+                e.printStackTrace();
+                this.response = e.getMessage();
+            }
+        }else{
+            response ="Invalid number of arguments-"+count;
+        }
+        return response;
+    }
+}
